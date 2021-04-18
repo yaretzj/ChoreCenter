b5e81163d7b57becbf4cdfcbcc7a00ce532657d5
yixiaoli <yixiaoli@cs.washington.edu>
2021-04-18 12:01:31 -0700

    added setting files

---
+++ Android/ChoreCenterApp/app/src/main/java/com/cse403chorecenter/chorecenterapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.cse403chorecenter.chorecenterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.cse403chorecenter.chorecenterapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Login button */
+    public void directToLogin(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, UserLogin.class);
+        // EditText editText = (EditText) findViewById(R.id.editText);
+        // String message = editText.getText().toString();
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
