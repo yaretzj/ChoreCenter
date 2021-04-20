f8ecde0774c50249f8170f3ce7a0dac77692847a
mllu777 <mllu777@uw.edu>
2021-04-19 20:53:00 -0700

    Implemented basic functionality in CreateChoreFragment

--- Android/ChoreCenterApp/app/src/main/java/com/cse403chorecenter/chorecenterapp/ui/create_chore/CreateChoreFragment.java
+++ Android/ChoreCenterApp/app/src/main/java/com/cse403chorecenter/chorecenterapp/ui/create_chore/CreateChoreFragment.java
@@ -1,9 +1,12 @@
 package com.cse403chorecenter.chorecenterapp.ui.create_chore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,28 +16,52 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cse403chorecenter.chorecenterapp.R;
+import com.cse403chorecenter.chorecenterapp.UserSignout;
 
 public class CreateChoreFragment extends Fragment {
     private CreateChoreViewModel createChoreViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        createChoreViewModel =
-                new ViewModelProvider(this).get(CreateChoreViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_create_chore, container, false);
-        final TextView textView = root.findViewById(R.id.text_create_chore);
-        createChoreViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        createChoreViewModel = new ViewModelProvider(this).get(CreateChoreViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_create_chore, container, false);
+        final TextView textView = view.findViewById(R.id.text_create_chore);
+
+        EditText editChoreName = (EditText) view.findViewById(R.id.editCreateChoreName);
+        EditText editChorePoints = (EditText) view.findViewById(R.id.editCreateChorePoints);
+
+        Button button = (Button) view.findViewById(R.id.button_create_chore);
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) { textView.setText(s); }
+            public void onClick(View view)
+            {
+
+
+                if(editChoreName == null || editChoreName.getText().toString().isEmpty()
+                                || editChorePoints == null || editChorePoints.getText().toString().isEmpty()) {
+                    textView.setText("please input both fields");
+                    return;
+                }
+
+                String choreName = editChoreName.getText().toString();
+                String chorePointsStr = editChorePoints.getText().toString();
+
+                // Check chorePoints is an integer
+                // TODO sanitise choreName input
+                try {
+                    int chorePointsInt = Integer.parseInt(chorePointsStr);
+                    editChoreName.setText("");
+                    editChorePoints.setText("");
+                } catch (NumberFormatException e) {
+                    textView.setText("please input a number for the chore points");
+                    return;
+                }
+
+                textView.setText("Chore " + choreName + " created");
+            }
         });
 
-        return root;
-    }
-
-    public void saveChore(View view) {
-
-    }
-    public void onClick() {
-
+        return view;
     }
 }
