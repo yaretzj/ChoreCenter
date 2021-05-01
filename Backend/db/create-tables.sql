74c69bfc88224cf462563d8d4271f74ddccb87cb
Dishad Chopra <dishad@cs.washington.edu>
2021-04-30 18:55:30 -0700

    Added timestamp triggers and default values.

--- Backend/db/create-tables.sql
+++ Backend/db/create-tables.sql
@@ -26,11 +26,11 @@ CREATE TABLE Chores (
     Status VARCHAR(15),
     AssignedTo VARCHAR(2048),
     Points INT,
-    CreatedTime INT,
-    AcceptedTime INT,
-    CompletedTime INT,
-    VerifiedTime INT,
-    LastUpdateTime INT,
+    CreatedTime DATETIME DEFAULT GETDATE(),
+    AcceptedTime DATETIME,
+    CompletedTime DATETIME,
+    VerifiedTime DATETIME,
+    LastUpdateTime DATETIME DEFAULT GETDATE(),
     FOREIGN KEY (ParentGoogleAccountId) REFERENCES Parents(GoogleAccountId),
     FOREIGN KEY (AssignedTo) REFERENCES Children(GoogleAccountId)
 );
@@ -41,8 +41,8 @@ CREATE TABLE Rewards (
     Name VARCHAR(255),
     Description VARCHAR(1023),
     Points INT,
-    CreatedTime INT,  -- Date stored as epoch timestamp
-    LastUpdateTime INT,
+    CreatedTime DATETIME DEFAULT GETDATE(),
+    LastUpdateTime DATETIME DEFAULT GETDATE(),
     FOREIGN KEY (ParentGoogleAccountId) REFERENCES Parents(GoogleAccountId)
 );
 
@@ -50,8 +50,22 @@ CREATE TABLE RewardRedemptionHistory (
     RewardId uniqueidentifier,
     ChildGoogleAccountId VARCHAR(2048),
     ParentGoogleAccountId VARCHAR(2048),
-    Timestamp INT,
+    RedeemedTime DATETIME DEFAULT GETDATE(),
     FOREIGN KEY (ParentGoogleAccountId) REFERENCES Parents(GoogleAccountId),
     FOREIGN KEY (ChildGoogleAccountId) REFERENCES Children(GoogleAccountId),
     FOREIGN KEY (RewardId) REFERENCES Rewards(RewardId),
 );
+
+CREATE TRIGGER tgr_lastupdatetime_chore
+ON Chores
+AFTER UPDATE AS
+  UPDATE Chores
+  SET LastUpdateTime = GETDATE()
+  WHERE ChoreId IN (SELECT DISTINCT ChoreId FROM Inserted);
+
+CREATE TRIGGER tgr_lastupdatetime_reward
+ON Rewards
+AFTER UPDATE AS
+  UPDATE Rewards
+  SET LastUpdateTime = GETDATE()
+  WHERE RewardId IN (SELECT DISTINCT RewardId FROM Inserted);
--- Backend/db/queries.py
+++ Backend/db/queries.py
@@ -3,15 +3,24 @@ from pypika import Query, Table, Field
 # Setup: run pip install pypika
 # To convert queries into raw SQL strings, use str(q) or q.get_sql()
 
-parents = Table('Parents')
-children = Table('Children')
-chores = Table('Chores')
-rewards = Table('Rewards')
-rewardRedemptionHistory = Table('RewardRedemptionHistory')
+parents = Table("Parents")
+children = Table("Children")
+chores = Table("Chores")
+rewards = Table("Rewards")
+rewardRedemptionHistory = Table("RewardRedemptionHistory")
 
-def createParent(name, email, tokenID, accountID):
-    q = Query.into(parents).insert(name, email, tokenID, accountID)
+
+def create_parent(name, email, token_id, account_id):
+    q = Query.into(parents).insert(name, email, token_id, account_id)
     return q
 
+
+def create_child(name, email, token_id, account_id, parent_code):
+    q = Query.into(children).insert(
+        name,
+        email,
+    )
+
+
 # Test to show example generated SQL string
-print(createParent('John Doe', 'jdoe@uw.edu', '333', '1234').get_sql())
+print(create_parent("John Doe", "jdoe@uw.edu", "333", "1234").get_sql())
