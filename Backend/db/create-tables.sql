154a45118730beea3864f8fcfb8444577f5bdb47
Dishad Chopra <dishad@cs.washington.edu>
2021-05-01 18:11:48 -0700

    Separated create table and triggers sql files, added script for creating tables and triggers.

--- Backend/db/create-tables.sql
+++ Backend/db/create-tables.sql
@@ -55,17 +55,3 @@ CREATE TABLE RewardRedemptionHistory (
     FOREIGN KEY (ChildGoogleAccountId) REFERENCES Children(GoogleAccountId),
     FOREIGN KEY (RewardId) REFERENCES Rewards(RewardId),
 );
-
-CREATE TRIGGER tgr_lastupdatetime_chore
-ON Chores
-AFTER UPDATE AS
-  UPDATE Chores
-  SET LastUpdateTime = GETDATE()
-  WHERE ChoreId IN (SELECT DISTINCT ChoreId FROM Inserted);
-
-CREATE TRIGGER tgr_lastupdatetime_reward
-ON Rewards
-AFTER UPDATE AS
-  UPDATE Rewards
-  SET LastUpdateTime = GETDATE()
-  WHERE RewardId IN (SELECT DISTINCT RewardId FROM Inserted);
