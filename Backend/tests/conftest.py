08e1e751feb8dd441b5232670f319272e54e93ed
Dishad Chopra <dishad.chopra@gmail.com>
2021-05-13 19:52:55 -0700

    Create pytest fixtures.

---
+++ Backend/tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app import app
+
+from db.connection import setup_db_conn
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def cursor():
+    conn = setup_db_conn()
+    yield conn.cursor()
+    conn.close()
