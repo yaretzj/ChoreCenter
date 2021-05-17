8ecd228b33507889dbf2eeec13530361b3cfa1de
mllu777 <mllu777@uw.edu>
2021-05-16 22:18:18 -0700

    Made the kid home page different from the parent home page. Added buttons to the kid home page to navigate to the SubmitChore and RedeemReward pages.

---
+++ Android/ChoreCenterApp/app/src/main/java/com/cse403chorecenter/chorecenterapp/ui/home_kid/HomeKidFragment.java
@@ -0,0 +1,46 @@
+package com.cse403chorecenter.chorecenterapp.ui.home_kid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.cse403chorecenter.chorecenterapp.R;
+
+public class HomeKidFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_kid, container, false);
+
+        // SubmitChore
+        Button bSubmitChore = (Button) view.findViewById(R.id.button_home_kid_submit_chore);
+        bSubmitChore.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.nav_submit_chore);
+            }
+        });
+
+        // Redeem Reward
+        Button bRedeemReward = (Button) view.findViewById(R.id.button_home_kid_redeem_reward);
+        bRedeemReward.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.nav_redeem_reward);
+            }
+        });
+
+        return view;
+    }
+}
