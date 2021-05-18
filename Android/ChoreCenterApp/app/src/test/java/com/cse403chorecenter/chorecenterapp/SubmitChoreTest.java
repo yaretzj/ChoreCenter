6c97da210919e19e9cecbe115302d345f4755fca
Yixiao Li <yixiaoli@uw.edu>
2021-05-18 14:52:11 -0700

    updated SubmitChoreTest

--- Android/ChoreCenterApp/app/src/test/java/com/cse403chorecenter/chorecenterapp/SubmitChoreTest.java
+++ Android/ChoreCenterApp/app/src/test/java/com/cse403chorecenter/chorecenterapp/SubmitChoreTest.java
@@ -1,5 +1,17 @@
 package com.cse403chorecenter.chorecenterapp;
 
+import com.cse403chorecenter.chorecenterapp.ui.redeem_reward.RedeemRewardFragment;
+import com.cse403chorecenter.chorecenterapp.ui.submit_chore.SubmitChoreFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class SubmitChoreTest {
     String jsonStr = "{\"Chores\": [" +
             "        {" +
@@ -32,4 +44,30 @@ public class SubmitChoreTest {
             "        }" +
             "    ]" +
             "}";
+
+    @Test
+    public void parseJSONTest() {
+        ArrayList<SubmitChoreFragment.ChoreModel> mDataset = new ArrayList<>();
+
+        try {
+            JSONObject jsonObj = new JSONObject(jsonStr);
+
+            // Getting JSON Array node
+            JSONArray chores = jsonObj.getJSONArray("Chores");
+
+            // looping through All Chores
+            for (int i = 0; i < chores.length(); i++) {
+                JSONObject c = chores.getJSONObject(i);
+                mDataset.add(new SubmitChoreFragment.ChoreModel(c.getString("Name"), c.getLong("Points"),
+                        c.getString("Description"), c.getString("ChoreId")));
+            }
+
+            assertEquals(mDataset.get(0).getName(), "Doing the dishes");
+            assertEquals(mDataset.get(0).getDescription(), "Doing all the dishes from yesturday");
+            assertEquals(mDataset.get(1).getPoints(), 2000);
+            assertEquals(mDataset.get(1).getId(), "A854430D-8025-4BB1-93A4-9630FDFAEC73");
+        } catch (final JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
