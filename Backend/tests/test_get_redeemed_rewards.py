b065bf67bcda2be7e2dc6efb57e50b2b55629add
Dishad Chopra <dishad.chopra@gmail.com>
2021-05-18 20:44:50 -0700

    Remove timestamp fields from get api test expected results.

--- Backend/tests/test_get_redeemed_rewards.py
+++ Backend/tests/test_get_redeemed_rewards.py
@@ -5,21 +5,18 @@ get_redeemed_rewards_parent_exp_json = {
             "Name": "Ice Cream",
             "Description": "Get an icecream cone after dinner",
             "ChildName": "Test Child",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
         {
             "RewardId": "11A2EDDE-9FDD-4CB2-AC05-CA84D3A0B03C",
             "Name": "Cash Exchange",
             "Description": "$10 per 10000 pts",
             "ChildName": "Test Child",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
         {
             "RewardId": "11A2EDDE-9FDD-4CB2-AC05-CA84D3A0B03C",
             "Name": "Cash Exchange",
             "Description": "$10 per 10000 pts",
             "ChildName": "Test Child",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
     ]
 }
@@ -30,19 +27,16 @@ get_redeemed_rewards_child_exp_json = {
             "RewardId": "81301234-93A1-4D59-9E2F-7F13D837A37E",
             "Name": "Ice Cream",
             "Description": "Get an icecream cone after dinner",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
         {
             "RewardId": "11A2EDDE-9FDD-4CB2-AC05-CA84D3A0B03C",
             "Name": "Cash Exchange",
             "Description": "$10 per 10000 pts",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
         {
             "RewardId": "11A2EDDE-9FDD-4CB2-AC05-CA84D3A0B03C",
             "Name": "Cash Exchange",
             "Description": "$10 per 10000 pts",
-            "RedeemedTime": "Sat, 15 May 2021 00:18:01 GMT",
         },
     ]
 }
@@ -61,10 +55,7 @@ def test_get_parent_history_valid(client):
 
     assert rv.status_code == 200
     assert rv.is_json
-    assert (
-        get_redeemed_rewards_parent_exp_json["RedeemedRewards"]
-        == rv.get_json()["RedeemedRewards"]
-    )
+    check_response_equality(get_redeemed_rewards_parent_exp_json, rv.get_json())
 
 
 def test_get_parent_history_invalid(client):
@@ -92,10 +83,7 @@ def test_get_child_history_valid(client):
 
     assert rv.status_code == 200
     assert rv.is_json
-    assert (
-        get_redeemed_rewards_child_exp_json["RedeemedRewards"]
-        == rv.get_json()["RedeemedRewards"]
-    )
+    check_response_equality(get_redeemed_rewards_child_exp_json, rv.get_json())
 
 
 def test_get_child_history_invalid(client):
@@ -108,3 +96,15 @@ def test_get_child_history_invalid(client):
     res_json = rv.get_json()
     exp_json = {"RedeemedRewards": []}
     assert res_json == exp_json
+
+
+def check_response_equality(exp_json, res_json):
+    exp_list, res_list = (
+        exp_json["RedeemedRewards"],
+        res_json["RedeemedRewards"],
+    )
+    exp_list.sort(key=lambda rr: rr["Name"])
+    res_list.sort(key=lambda rr: rr["Name"])
+    for exp_rr, res_rr in zip(exp_list, res_list):
+        for field in exp_rr:
+            assert exp_rr[field] == res_rr[field]
--- Backend/tests/test_get_rewards.py
+++ Backend/tests/test_get_rewards.py
@@ -1,27 +1,21 @@
 get_rewards_exp_json = {
     "Rewards": [
         {
-            "CreatedTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Description": "Get an icecream cone after dinner",
-            "LastUpdateTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Name": "Ice Cream",
             "ParentGoogleAccountId": "TestParent",
             "Points": 1500,
             "RewardId": "81301234-93A1-4D59-9E2F-7F13D837A37E",
         },
         {
-            "CreatedTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Description": "Pizza for dinner on Friday",
-            "LastUpdateTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Name": "Pizza Party",
             "ParentGoogleAccountId": "TestParent",
             "Points": 12000,
             "RewardId": "24191524-2CCB-46E3-973A-B9F3C6C8DA40",
         },
         {
-            "CreatedTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Description": "$10 per 10000 pts",
-            "LastUpdateTime": "Thu, 13 May 2021 23:10:45 GMT",
             "Name": "Cash Exchange",
             "ParentGoogleAccountId": "TestParent",
             "Points": 10000,
@@ -45,7 +39,7 @@ def test_get_rewards_parent(client):
 
     assert rv.status_code == 200
     assert rv.is_json
-    assert get_rewards_exp_json["Rewards"] == rv.get_json()["Rewards"]
+    check_response_equality(rv.get_json())
 
 
 def test_get_rewards_child_bad_request(client):
@@ -71,4 +65,13 @@ def test_get_rewards_child(client):
 
     assert rv.status_code == 200
     assert rv.is_json
-    assert get_rewards_exp_json["Rewards"] == rv.get_json()["Rewards"]
+    check_response_equality(rv.get_json())
+
+
+def check_response_equality(res_json):
+    exp_list, res_list = get_rewards_exp_json["Rewards"], res_json["Rewards"]
+    exp_list.sort(key=lambda reward: reward["Name"])
+    res_list.sort(key=lambda reward: reward["Name"])
+    for exp_reward, res_reward in zip(exp_list, res_list):
+        for field in exp_reward:
+            assert exp_reward[field] == res_reward[field]
