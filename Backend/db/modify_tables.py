c0b83e78e68577f4b8840a11024662160f346077
Phi Ton <pton@cs.washington.edu>
2021-05-20 18:29:57 -0700

    Made requested changes

--- Backend/db/modify_tables.py
+++ Backend/db/modify_tables.py
@@ -1,7 +1,7 @@
 import os
-import pyodbc
 import argparse
-from dotenv import dotenv_values
+from dotenv import dotenv_values, load_dotenv
+from db.connection import setup_db_conn
 
 # Drop tables action currently disabled - want to avoid dropping tables accidentally. You can
 # enable it by removing the commented out parts if you want to.
@@ -21,35 +21,19 @@ group.add_argument(
     help="Populates the database tables with the test data specified in create_test_data.sql.",
     action="store_true"
 )
-"""
 group.add_argument(
     "-d",
     "--drop",
     help="Drops the database tables using the commands in drop-tables.sql.",
     action="store_true"
 )
-"""
 args = parser.parse_args()
 
 BASEDIR = os.path.abspath(os.path.dirname(".."))
 config = dotenv_values((os.path.join(BASEDIR, ".env")))
-print(BASEDIR)
 
-server = config["DB_SERVER"]
-database = config["DB_NAME"]
-username = config["DB_USERNAME"]
-password = config["DB_PASSWORD"]
-
-conn = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-    + server
-    + ";DATABASE="
-    + database
-    + ";UID="
-    + username
-    + ";PWD="
-    + password
-)
+load_dotenv()
+conn = setup_db_conn()
 cursor = conn.cursor()
 
 if args.create:
@@ -77,7 +61,7 @@ elif args.populate:
         create_statements = f.read().split("\n\n")
     [cursor.execute(stmt) for stmt in create_statements]
     cursor.commit()
-    """
+
 elif args.drop:
     print("Dropping tables!")
 
@@ -87,7 +71,7 @@ elif args.drop:
         drop_statements = f.read().split("\n")
     [cursor.execute(stmt) for stmt in drop_statements]
     cursor.commit()
-    """
+
 else:
     print("Default, no args: create tables and populate with data.")
 
