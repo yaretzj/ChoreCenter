fa50cfd8e2f6d6cdd38838eacdcad21cbb14a1be
phiphphi <52836851+phiphphi@users.noreply.github.com>
2021-05-20 18:31:02 -0700

    Expanded test data with additional chores and reformatting (#58)
    * altered test data
    
    * Added arguments (non-functionaL) for modify_tables script
    
    * Added sql files to run for modify_tables
    
    * Added more chores to flesh out test date
    
    * Update get_chores test with new data
    
    * Merged in create-modify-tables and fixed naming bugs
    
    * Made requested changes
    
    Co-authored-by: Jaden Yaretz <yaretzj@cs.washington.edu>
    Co-authored-by: Phi Ton <pton@cs.washington.edu>

---
+++ Backend/db/clear_tables.sql
@@ -0,0 +1,9 @@
+DELETE FROM RewardRedemptionHistory;
+
+DELETE FROM Rewards;
+
+DELETE FROM Chores;
+
+DELETE FROM Children;
+
+DELETE FROM Parents;
---
+++ Backend/db/modify_tables.py
@@ -0,0 +1,97 @@
+import os
+import argparse
+from dotenv import dotenv_values, load_dotenv
+from db.connection import setup_db_conn
+
+# Drop tables action currently disabled - want to avoid dropping tables accidentally. You can
+# enable it by removing the commented out parts if you want to.
+
+# Command line arguments handling
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument(
+    "-c",
+    "--create",
+    help="Creates the database tables specified in create-tables.sql with the triggers in create-triggers.sql.",
+    action="store_true"
+)
+group.add_argument(
+    "-p",
+    "--populate",
+    help="Populates the database tables with the test data specified in create_test_data.sql.",
+    action="store_true"
+)
+group.add_argument(
+    "-d",
+    "--drop",
+    help="Drops the database tables using the commands in drop-tables.sql.",
+    action="store_true"
+)
+args = parser.parse_args()
+
+BASEDIR = os.path.abspath(os.path.dirname(".."))
+config = dotenv_values((os.path.join(BASEDIR, ".env")))
+
+load_dotenv()
+conn = setup_db_conn()
+cursor = conn.cursor()
+
+if args.create:
+    print("Creating tables!")
+
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create-tables.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create-triggers.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+elif args.populate:
+    # Note: this may not work yet until create_test_data format gets revamped
+    print("Clearing and adding test data!")
+
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create_test_data.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+
+elif args.drop:
+    print("Dropping tables!")
+
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "drop-tables.sql")
+    ) as f:
+        drop_statements = f.read().split("\n")
+    [cursor.execute(stmt) for stmt in drop_statements]
+    cursor.commit()
+
+else:
+    print("Default, no args: create tables and populate with data.")
+
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create-tables.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create-triggers.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+    with open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "create_test_data.sql")
+    ) as f:
+        create_statements = f.read().split("\n\n")
+    [cursor.execute(stmt) for stmt in create_statements]
+    cursor.commit()
+
+conn.close()
