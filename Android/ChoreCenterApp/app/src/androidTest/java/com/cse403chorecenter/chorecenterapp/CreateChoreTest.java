1bade917e5c8af9ef507304a06319653f8b02401
Yixiao Li <yixiaoli@uw.edu>
2021-05-23 17:19:14 -0700

    updated create chore test

--- Android/ChoreCenterApp/app/src/androidTest/java/com/cse403chorecenter/chorecenterapp/CreateChoreTest.java
+++ Android/ChoreCenterApp/app/src/androidTest/java/com/cse403chorecenter/chorecenterapp/CreateChoreTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -28,17 +29,15 @@ public class CreateChoreTest {
             Application application = ApplicationProvider.getApplicationContext();
             SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(application);
 
-
-            ViewInteraction vi = onView(withId(R.id.button_home_create_chore));
-            vi.perform(click());
+            onView(withId(R.id.button_home_create_chore)).perform(click());
 
             // directly click create button
             onView(withId(R.id.button_create_chore)).perform(click());
             onView(withId(R.id.text_create_chore)).check(matches(withText("please input the chore name and chore points")));
 
             // non-integer points input
-            onView(withId(R.id.editCreateChoreName)).perform(typeText("testing"));
-            onView(withId(R.id.editCreateChorePoints)).perform(typeText("1000.12"));
+            onView(withId(R.id.editCreateChoreName)).perform(typeText("testing")).perform(closeSoftKeyboard());
+            onView(withId(R.id.editCreateChorePoints)).perform(typeText("1000.12")).perform(closeSoftKeyboard());
             onView(withId(R.id.button_create_chore)).perform(click());
             onView(withId(R.id.text_create_chore)).check(matches(withText("please input a number for the chore points")));
         }
